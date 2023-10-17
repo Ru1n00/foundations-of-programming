159fcffc424e765026383153eebf1d2cf5d02a9b
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-10-17 23:49:48 +0600

    First program

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  // stdio = standard input output
+
+int main() {
+
+    printf("Hello World!"); // print function - prints to console (terminal) anything inside the parenthesis
+
+    return 0;  // return 0 - means the program ran successfully
+}
+
+// anything after // is a comment and is ignored by the compiler
