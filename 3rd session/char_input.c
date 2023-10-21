f6cf0fb6dc4e27b6c613d4dffb926dd7b0a51da2
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-10-21 23:01:44 +0600

    Add char_input

---
+++ 3rd session/char_input.c
@@ -0,0 +1,19 @@
+
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    int num1, num2;
+    char op; 
+        
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    printf("Enter the operator: ");
+    scanf(" %c", &op);  // add an space berfore %c when you want to take a character input in order to ignore the newline character from the previous input
+    
+    printf("num1 is %d and num2 is %d and the operator is %c", num1, num2, op);
+
+    return 0;
+}
+    
