e7a8f1563806d450cf2e25df0af6f1075a55e6b3
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-10-21 23:05:06 +0600

    Edit comment

--- 3rd session/char_input.c
+++ 3rd session/char_input.c
@@ -10,7 +10,7 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     printf("Enter the operator: ");
-    scanf(" %c", &op);  // add an space berfore %c when you want to take a character input in order to ignore the newline character from the previous input
+    scanf(" %c", &op);  // add an space berfore %c when you want to take a character input in order to ignore any error  (to ignore the newline character from the previous input)
     
     printf("num1 is %d and num2 is %d and the operator is %c", num1, num2, op);
 
